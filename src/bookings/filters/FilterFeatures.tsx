3d63fa88d79f8d3ad394956d3313250e23c7f192
Galobiano <151698267+Galobiano@users.noreply.github.com>
2025-09-29 16:42:05 +0800

    feat: enhance FilterFeatures component with optional divider and improved layout

--- src/bookings/filters/FilterFeatures.tsx
+++ src/bookings/filters/FilterFeatures.tsx
@@ -1,19 +1,21 @@
 import type { filterCategory } from '@/constants/filter';
+import { Separator } from '@/components/ui/separator';
 import { Checkbox } from '@/components/ui/checkbox';
 import { Label } from '@/components/ui/label';
+import { RadioGroup, RadioGroupItem } from '@/components/ui/radio-group';
 
 interface featuresProps {
 	feature: filterCategory;
+	showDivider?: boolean;
 }
-const FilterFeatures: React.FC<featuresProps> = ({ feature }) => {
+const FilterFeatures: React.FC<featuresProps> = ({ feature, showDivider }) => {
 	return (
 		<div>
-			<h1 className="text-[1.1rem] font-normal text-black">{feature.category}</h1>
-
+			<h1 className="my-3 text-[1.1rem] font-normal text-black">{feature.category}</h1>
 			{feature.type === 'checkbox' && (
 				<div className="grid grid-cols-2 space-y-2 text-black">
 					{feature.items.map((item, index) => (
-						<Label key={index} className="text-md">
+						<Label key={index} className="text-md font-normal">
 							<Checkbox className="size-6" /> {item}
 						</Label>
 					))}
@@ -21,14 +23,17 @@ const FilterFeatures: React.FC<featuresProps> = ({ feature }) => {
 			)}
 
 			{feature.type === 'radio' && (
-				<div className="grid grid-cols-2 space-y-2 text-black">
-					{feature.items.map((item, index) => (
-						<Label key={index} className="text-md">
-							<Checkbox className="size-6" /> {item}
-						</Label>
-					))}
-				</div>
+				<RadioGroup>
+					<div className="grid grid-cols-2 space-y-2 text-black">
+						{feature.items.map((item, index) => (
+							<Label key={index} className="text-md font-normal">
+								<RadioGroupItem className="size-6" value={item} /> {item}
+							</Label>
+						))}
+					</div>
+				</RadioGroup>
 			)}
+			{showDivider && <Separator className="my-4" />}
 		</div>
 	);
 };
