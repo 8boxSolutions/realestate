aa194aea25a1328777a64d7763602f4eaa95433d
Galobiano <151698267+Galobiano@users.noreply.github.com>
2025-10-11 20:52:52 +0800

    feat: added images + import component + added component

--- src/features/home/FeaturesProject.tsx
+++ src/features/home/FeaturesProject.tsx
@@ -1,11 +1,6 @@
-import image1 from '@/assets/homepage/featuresImage1.jpg';
-import image2 from '@/assets/homepage/featuresImage2.jpg';
-import image3 from '@/assets/homepage/featuresImage3.jpg';
 import featureLocationIcon from '@/assets/homepage/featuresLocationIcon.svg';
 import featurePriceTagIcon from '@/assets/homepage/featuresPriceTagIcon.svg';
-import circle1 from '@/assets/homepage/circle-1.svg';
-import circle2 from '@/assets/homepage/circle-2.svg';
-
+import GalleryGrid from '@/components/GalleryGrid';
 const FeaturesProject = () => {
 	return (
 		<section className="flex justify-between gap-2">
@@ -37,21 +32,7 @@ const FeaturesProject = () => {
 				</div>
 			</div>
 
-			<div className="relative hidden lg:block">
-				<div className="absolute -top-5 -left-20 flex w-full justify-center">
-					<img src={circle1} alt="" className="size-50" />
-				</div>
-				<div className="relative z-10 mt-10 flex gap-10">
-					<img src={image1} alt="image-1" className="mt-10 h-120 w-70 rounded-4xl object-cover" />
-					<div className="flex flex-col gap-5">
-						<img src={image2} alt="image-2" className="w-120 rounded-4xl object-cover" />
-						<img src={image3} alt="image-3" className="w-120 rounded-4xl object-cover" />
-						<div className="-mt-14 mr-4 flex justify-end">
-							<img src={circle2} alt="icon" />
-						</div>
-					</div>
-				</div>
-			</div>
+			<GalleryGrid />
 		</section>
 	);
 };
---
+++ src/pages/resources/propertyGuidelines/PropertyGuidelines.tsx
@@ -0,0 +1,35 @@
+import bg from '@/assets/guidelines/img-bg.jpg';
+import img from '@/assets/guidelines/img-bg.jpg';
+import GuidelinesAndImages from './GuidelinesAndImages';
+import PropertyList from '@/features/home/PropertyList';
+const PropertyGuidelines = () => {
+	return (
+		<div className="px-10 py-10">
+			<div
+				className="relative flex min-h-96 items-center justify-center rounded-2xl"
+				style={{
+					backgroundImage: `url(${bg})`,
+					backgroundSize: 'cover',
+					backgroundRepeat: 'no-repeat',
+					backgroundPosition: 'center',
+				}}
+			>
+				<div className="relative z-10 space-y-3 px-4 text-center text-white">
+					{' '}
+					<h1 className="text-center text-3xl text-white sm:text-5xl lg:text-7xl">Real Estate Property Guidelines</h1>
+					<p className="sm:text-[1rem] lg:text-xl">
+						Explore properties that match your lifestyle—modern, affordable, and built for you. Find the perfect place
+						to start your next chapter.
+					</p>
+				</div>
+
+				{/* BACKGROUND-COLOR */}
+				<div className="absolute inset-0 rounded-2xl bg-[#39000073]/45"></div>
+			</div>
+			<GuidelinesAndImages />
+			<PropertyList />
+		</div>
+	);
+};
+
+export default PropertyGuidelines;
