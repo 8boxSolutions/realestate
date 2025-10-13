6f0dfc19cee123e953eee36355c516460f77c7d3
Galobiano <151698267+Galobiano@users.noreply.github.com>
2025-10-13 11:42:27 +0800

    feat: add data for all properties

--- src/constants/housePropertyData.ts
+++ src/constants/housePropertyData.ts
@@ -5,6 +5,7 @@ import img4 from '@/assets/apartment/img4.png';
 import img5 from '@/assets/lots/img-5.png';
 import img6 from '@/assets/commercials/img6.png';
 import img7 from '@/assets/imgs/sampleHouseAndLot.png';
+
 export interface PropertyDetails {
 	id: string;
 	squareFeet: number;
@@ -100,6 +101,73 @@ export const propertiesCommercialsForSale: PropertyDetails[] = Array.from({ leng
 }));
 
 export const propertiesHouseAndLotForSale: PropertyDetails[] = Array.from({ length: 6 }, (_, i) => ({
+	id: (i + 1).toString(),
+	squareFeet: 1000,
+	bed: 5,
+	user: 'John Smith',
+	garages: 1,
+	bath: 3,
+	price: 2500000,
+	time: '2 days ago',
+	image: img7,
+	location: 'Quezon City, Manila 1234',
+	title: `17 Quaker Road, Manila #${i + 1}`,
+}));
+
+// ALL PROPERTIES
+
+export const propertiesCondoAll: PropertyDetails[] = Array.from({ length: 3 }, (_, i) => ({
+	id: (i + 1).toString(),
+	squareFeet: 1000,
+	unit: 4,
+	user: 'John Smith',
+	garages: 1,
+	bath: 3,
+	price: 2500000,
+	time: '2 days ago',
+	image: img3,
+	location: 'Quezon City, Manila 1234',
+	title: `17 Quaker Road, Manila #${i + 1}`,
+}));
+
+export const propertiesApartmentAll: PropertyDetails[] = Array.from({ length: 3 }, (_, i) => ({
+	id: (i + 1).toString(),
+	squareFeet: 1000,
+	bed: 5,
+	user: 'John Smith',
+	garages: 2,
+	bath: 3,
+	price: 2500000,
+	time: '2 days ago',
+	image: img4,
+	location: 'Quezon City, Manila 1234',
+	title: `17 Quaker Road, Manila #${i + 1}`,
+}));
+
+export const propertiesLotsAll: PropertyDetails[] = Array.from({ length: 3 }, (_, i) => ({
+	id: (i + 1).toString(),
+	squareFeet: 480000,
+	user: 'John Smith',
+	price: 2500000,
+	time: '2 days ago',
+	image: img5,
+	location: 'Quezon City, Manila 1234',
+	title: `17 Quaker Road, Manila #${i + 1}`,
+}));
+
+export const propertiesCommercialsAll: PropertyDetails[] = Array.from({ length: 3 }, (_, i) => ({
+	id: (i + 1).toString(),
+	squareFeet: 480000,
+	unit: 14,
+	user: 'John Smith',
+	price: 2500000,
+	time: '2 days ago',
+	image: img6,
+	location: 'Quezon City, Manila 1234',
+	title: `17 Quaker Road, Manila #${i + 1}`,
+}));
+
+export const propertiesHouseAndLotAll: PropertyDetails[] = Array.from({ length: 3 }, (_, i) => ({
 	id: (i + 1).toString(),
 	squareFeet: 1000,
 	bed: 5,
