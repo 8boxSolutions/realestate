e6363875afd276f1bd44db4a32a41f73f6785930
Galobiano <151698267+Galobiano@users.noreply.github.com>
2025-10-17 15:32:31 +0800

    feat: import component

--- src/App.tsx
+++ src/App.tsx
@@ -8,11 +8,11 @@ import CommercialForSale from './pages/buy/CommercialForSale';
 import AllProptertiesForSale from './pages/buy/AllProptertiesForSale';
 
 // FOR RENT
-import CondoForRent from './pages/rent/CondoForRent';
-import ApartmentForRent from './pages/rent/ApartmentForRent';
-import LotsForRent from './pages/rent/LotsForRent';
-import CommercialForRent from './pages/rent/CommercialForRent';
-import AllPropertiesForRent from './pages/rent/AllPropertiesForRent';
+// import CondoForRent from './pages/rent/CondoForRent';
+// import ApartmentForRent from './pages/rent/ApartmentForRent';
+// import LotsForRent from './pages/rent/LotsForRent';
+// import CommercialForRent from './pages/rent/CommercialForRent';
+// import AllPropertiesForRent from './pages/rent/AllPropertiesForRent';
 
 import NewDevelopmentsSection from './pages/NewDevelopmentsSection';
 import ForeclosuresSection from './pages/ForeclosuresSection';
@@ -26,36 +26,50 @@ import FavoriteSection from './pages/favorite/FavoriteSection';
 
 import { BuyHouseAndLot, Home } from '@/pages';
 
+// BUY DETAILS
+import HouseForSaleDetails from './features/home/buyDetails/HouseForSaleDetails';
+import CondoForSaleDetails from './features/home/buyDetails/CondoForSaleDetails';
+import UnitsForSaleDetails from './features/home/buyDetails/UnitsForSaleDetails';
+import ApartmentsForSaleDetails from './features/home/buyDetails/ApartmentsForSaleDetails';
+import LotForSaleDetails from './features/home/buyDetails/LotForSaleDetails';
+
 function App() {
 	return (
 		<BrowserRouter>
 			<Routes>
 				<Route element={<WebLayout />}>
 					<Route path="/" element={<Home />} />
-					<Route path="/house-and-lot/buy" element={<BuyHouseAndLot />} />
-
 					{/* BUY */}
+					<Route path="/house-and-lot/buy" element={<BuyHouseAndLot />} />
+					<Route path="/house-and-lot/buy/:houseId" element={<HouseForSaleDetails />} />
+
 					<Route path="/condo-for-sale" element={<CondoForSale />} />
+					<Route path="/condo-for-sale/:condoId" element={<CondoForSaleDetails />} />
+
 					<Route path="/apartments-for-sale" element={<ApartmentForSale />} />
+					<Route path="/apartments-for-sale/:apartmentId" element={<ApartmentsForSaleDetails />} />
+
 					<Route path="/lots-for-sale" element={<LotsForSale />} />
+					<Route path="/lots-for-sale/:lotId" element={<LotForSaleDetails />} />
+
 					<Route path="/commercial-unit-for-sale" element={<CommercialForSale />} />
+					<Route path="/commercial-unit-for-sale/:unitId" element={<UnitsForSaleDetails />} />
+
 					<Route path="/all-properties-for-sale" element={<AllProptertiesForSale />} />
 
 					{/* RENT */}
-					<Route path="/condo-for-rent" element={<CondoForRent />} />
+					{/* <Route path="/condo-for-rent" element={<CondoForRent />} />
 					<Route path="/apartments-for-rent" element={<ApartmentForRent />} />
 					<Route path="/lots-for-rent" element={<LotsForRent />} />
 					<Route path="/commercial-unit-for-rent" element={<CommercialForRent />} />
-					<Route path="/all-properties-for-rent" element={<AllPropertiesForRent />} />
+					<Route path="/all-properties-for-rent" element={<AllPropertiesForRent />} /> */}
 
 					<Route path="/new-developments" element={<NewDevelopmentsSection />} />
 					<Route path="/foreclosures" element={<ForeclosuresSection />} />
-
 					{/* RESOURCES */}
 					<Route path="/journal" element={<Journal />} />
 					<Route path="/loan-calculator" element={<LoanCalculator />} />
 					<Route path="/property-guides" element={<PropertyGuidelines />} />
-
 					<Route path="/favorite" element={<FavoriteSection />} />
 				</Route>
 			</Routes>
