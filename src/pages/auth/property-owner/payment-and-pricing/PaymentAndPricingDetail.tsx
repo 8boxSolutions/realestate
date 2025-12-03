c1c60c2a04dd66f28bf68bd6d3fc879f9b99284f
Galobiano <151698267+Galobiano@users.noreply.github.com>
2025-12-03 10:41:42 +0800

    feat: import component

--- src/pages/auth/property-owner/payment-and-pricing/PaymentAndPricingDetail.tsx
+++ src/pages/auth/property-owner/payment-and-pricing/PaymentAndPricingDetail.tsx
@@ -1,12 +1,18 @@
 import React from 'react';
 import Pricing from './Pricing';
+import Discount from './Discount';
 
 interface subStepProps {
 	currentSubStep?: number;
 }
 
 const PaymentAndPricingDetail: React.FC<subStepProps> = ({ currentSubStep }) => {
-	return <div>{currentSubStep === 0 && <Pricing />}</div>;
+	return (
+		<div>
+			{currentSubStep === 0 && <Pricing />}
+			{currentSubStep === 1 && <Discount />}
+		</div>
+	);
 };
 
 export default PaymentAndPricingDetail;
