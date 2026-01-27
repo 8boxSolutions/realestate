c348cabb774c67c2e810cc86e86a8ae7a2431ddd
Lanz45 <lancemalbas5@gmail.com>
2026-01-27 16:38:04 +0800

    added Home and Favorite page

--- src/pages/client/ClientFavorites.tsx
+++ src/pages/client/ClientFavorites.tsx
@@ -1,11 +1,19 @@
+import PropertyGrid from '@/components/PropertyGrid';
 import React from 'react';
 
 export const ClientFavorites: React.FC = () => {
   return (
-    <div>
-      <h1>Client Favorites</h1>
-    </div>
-  );
+		<div className="px-4 pt-10 pb-15 sm:px-8 lg:px-20">
+			{/* Header Section */}
+			<div className="flex flex-col items-center space-y-5 text-center">
+				<h1 className="text-5xl font-semibold text-primary">Favorite</h1>
+				<p className="max-w-xl text-sm text-black sm:text-base">
+					Leo morbi faucibus mattis pharetra tellus velit ultricies duis rhoncus
+				</p>
+			</div>
+			<PropertyGrid />
+		</div>
+	);
 };
 
 export default ClientFavorites;
