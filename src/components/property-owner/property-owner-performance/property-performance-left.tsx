359de3eb0cb5b028838687e2abad6e6a67080ccb
Darantinao <jmjrjun024@gmail.com>
2026-02-01 23:17:20 +0800

    property-performance left

---
+++ src/components/property-owner/property-owner-performance/property-performance-left.tsx
@@ -0,0 +1,60 @@
+
+function PropertyPerformanceLeft() {
+    return (
+        <>
+            <div className="relative w-full lg:w-2/10 h-100 break-words">
+                <div className="pl-15 space-y-10">
+                    <h1 className="text-3xl font-bold">Performance</h1>
+                    <div className="inline-flex gap-4 items-center">
+                        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="red" className="size-6 bg-red-300 rounded-full w-13 h-13 p-3">
+                            <path strokeLinecap="round" strokeLinejoin="round" d="M21.75 6.75v10.5a2.25 2.25 0 0 1-2.25 2.25h-15a2.25 2.25 0 0 1-2.25-2.25V6.75m19.5 0A2.25 2.25 0 0 0 19.5 4.5h-15a2.25 2.25 0 0 0-2.25 2.25m19.5 0v.243a2.25 2.25 0 0 1-1.07 1.916l-7.5 4.615a2.25 2.25 0 0 1-2.36 0L3.32 8.91a2.25 2.25 0 0 1-1.07-1.916V6.75" />
+                        </svg>
+                        <div className="w-35">
+                            <p className="text-gray-400 text-xs">Inquiries Received</p>
+                            <h3 className="font-bold">1236</h3>
+                        </div>
+                    </div>
+                    <div className="inline-flex gap-4 items-center">
+                        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="red" className="size-6 bg-red-300 rounded-full w-13 h-13 p-3">
+                            <path strokeLinecap="round" strokeLinejoin="round" d="M2.036 12.322a1.012 1.012 0 0 1 0-.639C3.423 7.51 7.36 4.5 12 4.5c4.638 0 8.573 3.007 9.963 7.178.07.207.07.431 0 .639C20.577 16.49 16.64 19.5 12 19.5c-4.638 0-8.573-3.007-9.963-7.178Z" />
+                            <path strokeLinecap="round" strokeLinejoin="round" d="M15 12a3 3 0 1 1-6 0 3 3 0 0 1 6 0Z" />
+                        </svg>
+                        <div className="w-35">
+                            <p className="text-gray-400 text-xs">Scheduled Viewings</p>
+                            <h3 className="font-bold">300</h3>
+                        </div>
+                    </div>
+                    <div className="inline-flex gap-4 items-center">
+                        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="red" className="size-6 bg-red-300 rounded-full w-13 h-13 p-3">
+                            <path strokeLinecap="round" strokeLinejoin="round" d="M10.05 4.575a1.575 1.575 0 1 0-3.15 0v3m3.15-3v-1.5a1.575 1.575 0 0 1 3.15 0v1.5m-3.15 0 .075 5.925m3.075.75V4.575m0 0a1.575 1.575 0 0 1 3.15 0V15M6.9 7.575a1.575 1.575 0 1 0-3.15 0v8.175a6.75 6.75 0 0 0 6.75 6.75h2.018a5.25 5.25 0 0 0 3.712-1.538l1.732-1.732a5.25 5.25 0 0 0 1.538-3.712l.003-2.024a.668.668 0 0 1 .198-.471 1.575 1.575 0 1 0-2.228-2.228 3.818 3.818 0 0 0-1.12 2.687M6.9 7.575V12m6.27 4.318A4.49 4.49 0 0 1 16.35 15m.002 0h-.002" />
+                        </svg>
+                        <div className="w-35">
+                            <p className="text-gray-400 text-xs">Offer Made</p>
+                            <h3 className="font-bold">257</h3>
+                        </div>
+                    </div>
+                    <div className="inline-flex gap-4 items-center">
+                        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="red" className="size-6 bg-red-300 rounded-full w-13 h-13 p-3">
+                            <path strokeLinecap="round" strokeLinejoin="round" d="M3 3v1.5M3 21v-6m0 0 2.77-.693a9 9 0 0 1 6.208.682l.108.054a9 9 0 0 0 6.086.71l3.114-.732a48.524 48.524 0 0 1-.005-10.499l-3.11.732a9 9 0 0 1-6.085-.711l-.108-.054a9 9 0 0 0-6.208-.682L3 4.5M3 15V4.5" />
+                        </svg>
+                        <div className="w-35">
+                            <p className="text-gray-400 text-xs">Property Listed</p>
+                            <h3 className="font-bold">293</h3>
+                        </div>
+                    </div>
+                    <div className="inline-flex gap-4 items-center">
+                        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="red" className="size-6 bg-red-300 rounded-full w-13 h-13 p-3">
+                            <path strokeLinecap="round" strokeLinejoin="round" d="M15.75 9V5.25A2.25 2.25 0 0 0 13.5 3h-6a2.25 2.25 0 0 0-2.25 2.25v13.5A2.25 2.25 0 0 0 7.5 21h6a2.25 2.25 0 0 0 2.25-2.25V15M12 9l-3 3m0 0 3 3m-3-3h12.75" />
+                        </svg>
+                        <div className="w-35">
+                            <p className="text-gray-400 text-xs">Closed Deals / Sales</p>
+                            <h3 className="font-bold">300</h3>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </>
+    )
+}
+
+export default PropertyPerformanceLeft;
--- src/pages/property-pages/property-performance.tsx
+++ src/pages/property-pages/property-performance.tsx
@@ -1,6 +1,20 @@
+import PropertyPerformanceLeft from "@/components/property-owner/property-owner-performance/property-performance-left";
 
 const PropertyPerformance = () => {
-    return <div>Property Performance Page</div>;
+    return (
+        <>
+            <div className="inline-flex w-full flex-col lg:flex-row">
+                {/* Left Side */}
+                <PropertyPerformanceLeft />
+                {/* Middle */}
+                <div className="relative w-full lg:w-5/10 bg-gray-100 h-100">
+                </div>
+                {/* Right Side */}
+                <div className="relative w-full lg:w-3/10 h-100 bg-blue-800">
+                </div>
+            </div>
+        </>
+    )
 }   
 
 export default PropertyPerformance;
